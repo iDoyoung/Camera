2d0c003e084939471a69261d14f86b3007259a98
iDoyoung <ido.young@icloud.com>
2025-02-24 14:53:24 +0900

    Initial Commit

---
+++ LoopCam/LoopCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopCamApp.swift
+//  LoopCam
+//
+//  Created by Doyoung on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct LoopCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
